24043d5ecca4efa255948163d545a91c0213d3dd
Naren Sarayu Manoj <nsm@NSM-MacBook-Pro-M1-2020.local>
2026-05-02 16:44:27 -0400

    add optimism

--- run_gaussian.sh
+++ run_gaussian.sh
@@ -1,4 +1,4 @@
-for a in 4.0 8.0 16.0 32.0 64.0; do
+for a in 0.9 0.8 0.7; do
     torchrun --standalone --nproc_per_node=$(nvidia-smi -L | wc -l) \
         records/track_3_optimization/train_gpt_simple.py \
         --lookahead_alpha $a --lookahead_mode gaussian
